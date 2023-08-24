b0bc4f7a2c36cf340e5154d5abdce3632f7771ad
Tobias Beers <t.beers@nki.nl>
2023-08-24 13:53:26 +0200

    Addded requirements and configuration management section. Had trouble with Vuepress, so I cannot check if the final output is working properly

--- docs/.vuepress/bar/sidebar.ts
+++ docs/.vuepress/bar/sidebar.ts
@@ -10,7 +10,8 @@ export const sidebarImport: SidebarConfig = {
         '/fieldnorm/life_cycle.md',
         '/fieldnorm/verification_validation.md',
         '/fieldnorm/risk_management.md',
-        '/fieldnorm/feedback.md'
+        '/fieldnorm/feedback.md',
+        '/fieldnorm/requirements_configuration.md'
       ],
     },
   ]
